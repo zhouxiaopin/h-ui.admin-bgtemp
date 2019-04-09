7e799a2b548066a5af16bba55cc734c3794b61ae
zcp <1156693737@qq.com>
2019-04-09 14:26:18 +0800

    添加了机构模块

---
+++ src/main/java/cn/sk/huiadminbgtemp/business/mapper/OrganMapper.java
@@ -0,0 +1,11 @@
+package cn.sk.huiadminbgtemp.business.mapper;
+
+import cn.sk.huiadminbgtemp.base.mapper.IBaseMapper;
+import cn.sk.huiadminbgtemp.business.pojo.OrganCustom;
+import cn.sk.huiadminbgtemp.business.pojo.OrganQueryVo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganMapper extends IBaseMapper<OrganCustom, OrganQueryVo> {
+
+}
